eb6a3267b4c1348bd77cf233f147b7b7bbe21d0b
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-12-16 13:13:19 +0100

    working magazine prototype with all song elements; curator and title/back pages are still missing

--- Magazin/magazinbasil.jsx
+++ Magazin/magazinbasil.jsx
@@ -1,15 +1,24 @@
 #includepath "~/Documents/;%USERPROFILE%Documents";
 #include "basiljs/bundle/basil.js";
 
-// Constants
+// Grid Settings
 var gridX = [0, 49.606, 99.213, 148.819, 198.425, 248.031, 297.638, 347.244, 396.85, 446.457, 496.063, 545.669, 630.709, 680.315, 729.921, 779.528, 829.134, 878.74, 928.346, 977.953, 1027.559, 1077.165, 1126.772];
 var gridY = [0, 73.228, 146.457, 219.685, 292.913, 366.142, 366.142, 439.37, 512.598, 585.827, 659.055, 732.283, 805.512];
-var font = "News Gothic";
 var commentBlockWidth = 4;
 var gridWidth = 49.607;
 var gridHeight = 73.229;
 var gridGap = 14.173;
 var maxYStart = 10;
+
+// Font Settings
+var font = "Oswald";
+
+// Image Settings
+var coverSize = 250;
+var artworkSize = 450;
+
+
+
 
 // Main function
 function draw() {
@@ -31,7 +40,7 @@ function draw() {
   // Kurator
 
   // TODO: build Kurator here!
-
+ 
 
 
   // Content
@@ -41,13 +50,15 @@ function draw() {
 
   	b.addPage();
 
+  	placeCover(entries[i].coverUrl, i);
+  	placeArtwork(entries[i].artworkUrl, i);
+
   	placeComments(entries[i].comments, 0);
   	placeSongtitle(entries[i].songtitle);
-  	// placeBandname(entries[i].bandname);
-  	// placeComments(entries[i].comments, false, 0);
+  	placeBandname(entries[i].bandname);
   	
-  	// placeArtwork(entries[i].artworkUrl);
-  	// placeCover(entries[i].coverUrl);
+  	placeDate(i+1);
+  	
   	
   	b.addPage();
   }
@@ -62,28 +73,18 @@ function draw() {
 
 function placeComments(comments, titleYStart) {
 
-	// Basic Styling
-	b.textFont(font, "Regular");
-	b.textSize(11);
+	// Text Styling
+	b.textFont(font, "Light");
+	b.textSize(10);
+	b.textLeading(13);
+	b.textTracking(10);
 	b.textAlign(Justification.FULLY_JUSTIFIED);
 
-	// Test Content
-	// var totalComments = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
-
-	// var totalComments = comments[0].comment + "n" + comments[1].comment + "n";
-
+	// Combine all comments
 	var totalComments = '';
-
-	// for (var key in comments) {
-	// 	totalComments = totalComments.concat(key.comment + "n" + "n");
-	// }
-
 	b.forEach(comments, function(item, i) {
 		totalComments = totalComments + item.name + "//" + "\n" + item.comment + "\n" + "\n";
 	});
-
-	
-
 
 	// b.rotate(b.PI*1.5);
 
@@ -137,13 +138,19 @@ function placeComments(comments, titleYStart) {
 }
 
 function placeBandname(bandname) {
+
+	// Text Styling
 	b.textFont(font, "Bold");
 	b.textSize(100);
+	b.textLeading(Leading.AUTO);
+	b.textTracking(0);
 	b.textAlign(Justification.LEFT_ALIGN);
 
-	// Texteingabe
-	var randomX = b.round(b.random(0, 7));
-	var randomY = b.round(b.random(0,5));
+	// Text positioning
+	var randomX = b.round(b.random(0, 8));
+	var randomY = b.round(b.random(0, 5));
+
+	// Frame
 	var myFrame = b.text(bandname.toUpperCase(), gridX[randomX] - b.width, gridY[randomY], b.width * 2 - gridX[randomX], b.height);
 
 	// Einzug auf der ersten Linie
@@ -151,28 +158,67 @@ function placeBandname(bandname) {
 }
 
 function placeSongtitle(songtitle) {
-	b.textFont(font, "Bold");
-	b.textSize(100);
+
+	// Text Styling
+	b.textFont(font, "Regular");
+	b.textSize(40);
+	b.textLeading(Leading.AUTO);
+	b.textTracking(0);
 	b.textAlign(Justification.LEFT_ALIGN);
 
-	// Texteingabe
-	var randomX = b.round(b.random(0, 7));
-	var randomY = b.round(b.random(0,5));
+	// Text positioning
+	var randomX = b.round(b.random(0, 9));
+	var randomY = b.round(b.random(0, 5));
+
+	// Frame
 	var myFrame = b.text(songtitle.toUpperCase(), gridX[randomX] - b.width, gridY[randomY], b.width * 2 - gridX[randomX], b.height);
 
 	// Einzug auf der ersten Linie
 	// b.typo(myFrame, 'firstLineIndent', 100);
 }
 
-function placeArtwork(artworkURL) {
+function placeArtwork(artworkURL, id) {
+
+	// Download image
+	var imagePath = "artwork/artwork_" + id + ".jpg";
+	// b.download(artworkURL, imagePath);
+
+	// Place image
+	var imageX = gridX[b.round(b.random(0, 16))] - b.width;
+	var imageY = gridY[b.round(b.random(0, 6))];
+	var image = b.image(imagePath, imageX, imageY, artworkSize, artworkSize);
+	b.opacity(image, 80);
 
 }
 
-function placeCover(coverURL) {
+function placeCover(coverURL, id) {
 
+	// Download image
+	var imagePath = "cover/cover_" + id + ".jpg";
+	// b.download(coverURL, imagePath);
+
+	// Place image
+	var imageX = gridX[b.round(b.random(0, 17))] - b.width;
+	var imageY = gridY[b.round(b.random(0, 8))];
+	var image = b.image(imagePath, imageX, imageY, coverSize, coverSize);
+	b.opacity(image, 50);
 }
 
-function placeDate() {
+function placeDate(dayInt) {
+
+	// Text Styling
+	b.textFont(font, "Regular");
+	b.textSize(31);
+	b.textLeading(40);
+	b.textTracking(0);
+	b.textAlign(Justification.RIGHT_ALIGN);
+
+	var positionX = b.width - 81;
+	var positionY = 9;
+
+	var dateString = "0" + dayInt + "  " + "12" + "\n" + "16";
+	var myFrame = b.text(dateString, positionX, positionY, b.width - positionX - 12, 100);
+
 
 }
 
